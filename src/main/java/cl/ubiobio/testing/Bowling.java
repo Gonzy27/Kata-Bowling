5555ff2cdc243372e81228d97ced3d302b59c56d
Gonzalo Espinoza <gonzaloespinoza.alarcon@gmail.com>
2017-04-26 23:53:45 -0300

    Test con spare y correcion de simbolos

--- src/main/java/cl/ubiobio/testing/Bowling.java
+++ src/main/java/cl/ubiobio/testing/Bowling.java
@@ -2,22 +2,37 @@ package cl.ubiobio.testing;
 
 public class Bowling {
 	int pins = 0;
+	boolean primerRoll = true;
 	boolean strike = false;
+	boolean spare = false;
+	int previousRoll = 0;
 	int contStrike = 0;
 
-	public void roll(int pinsRoll) {
-		pins += pinsRoll;
-		if (strike == true) {
-			pins += pinsRoll;
-			contStrike += 1;
-		}
-		if(contStrike == 2){
-			contStrike = 0;
+	public void roll(char pinsRoll) {
+		if (pinsRoll == 'X') {
 			strike = true;
+			pins += 10;
+		} else if (pinsRoll == '/') {
+			spare = true;
+			pins += (10 - previousRoll);
+		} else {
+			if (pinsRoll == '/')
+				System.out.println("asd");
+			pins += Character.getNumericValue(pinsRoll);
+			previousRoll = Character.getNumericValue(pinsRoll);
+			if (strike == true) {
+				pins += Character.getNumericValue(pinsRoll);
+				contStrike += 1;
+			}
+			if (contStrike == 2) {
+				contStrike = 0;
+				strike = false;
+			}
+			if (spare == true){
+				pins += Character.getNumericValue(pinsRoll);
+				spare = false;
+			}
 		}
-		if (pinsRoll == 10)
-			strike = true;
-
 	}
 
 	public int score() {
--- src/test/java/cl/ubiobio/testing/TestBowling.java
+++ src/test/java/cl/ubiobio/testing/TestBowling.java
@@ -9,11 +9,11 @@ public class TestBowling {
 	@Test
 	public void firstRollThreeAndSecondRollFiveShouldBeEight() {
 		Bowling game = new Bowling();
-		game.roll(3);
-		game.roll(5);
+		game.roll('3');
+		game.roll('5');
 		int result = game.score();
 		for (int i = 0; i < 18; i++)
-			game.roll(0);
+			game.roll('0');
 		assertEquals(8, result);
 	
 	}
@@ -21,12 +21,12 @@ public class TestBowling {
 	@Test
 	public void firstRollThreeSecondRollSevenThirdRollSevenAndFourRollTwoShouldBeSevenTeen() {
 		Bowling game = new Bowling();
-		game.roll(3);
-		game.roll(5);
-		game.roll(7);
-		game.roll(2);
+		game.roll('3');
+		game.roll('5');
+		game.roll('7');
+		game.roll('2');
 		for (int i = 0; i < 16; i++)
-			game.roll(0);
+			game.roll('0');
 		int result = game.score();
 		assertEquals(17, result);
 
@@ -35,13 +35,25 @@ public class TestBowling {
 	@Test
 	public void firstRollStrikeSecondRollThreeAndTrirdRollFiveShouldBeTwentySix() {
 		Bowling game = new Bowling();
-		game.roll(10);
-		game.roll(3);
-		game.roll(5);
+		game.roll('X');
+		game.roll('3');
+		game.roll('5');
 		for (int i = 0; i < 16; i++)
-			game.roll(0);
+			game.roll('0');
 		int result = game.score();
 		assertEquals(26, result);
 	}
-		
+	
+	@Test 
+	public void firstRollFiveSecondRollFiveSoSpareThirdRollFiveAndFourRollTwoShouldBeTwentyTwo(){
+		Bowling game = new Bowling();
+		game.roll('5');
+		game.roll('/');
+		game.roll('5');
+		game.roll('2');
+		for (int i = 0; i < 16; i++)
+			game.roll('0');
+		int result = game.score();
+		assertEquals(22, result);
+	}
 }
